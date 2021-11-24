caee828bff86319dbb575aeb025396a8c45ed0c1
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-11-24 09:16:31 +0000

    [Finish] rails-all

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '24386d404af8b89856cae69a7232b7117fd92951ddba2e2a7ad3d08a71247e8d6d96f7812f29d99d5c03b4ee0a8a830dfb3d9df6318b82f44f80a0c202cbcf4a'
